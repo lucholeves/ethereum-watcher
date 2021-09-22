304361b8c8b4b979aeac8fab6a5ef792015c7d47
Lucho Leves <lucianoleves@gmail.com>
2021-09-22 12:21:54 -0300

    Add docker and celery
    - Add docker-compose file
    - Add rabbitmq service, circus service, django service
    - Update settings
    - Fix paths

--- django/project/app/config/urls.py
+++ django/project/app/config/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("project.app.web_client.urls")),
+    path("", include("app.web_client.urls")),
     path("admin/", admin.site.urls),
     # API REST URL
-    path("api/", include("project.app.blockchain_eth.urls")),
+    path("api/", include("app.blockchain_eth.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
