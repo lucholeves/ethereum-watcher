9d046934328ae7e37a033f9dbe8237ca091f75af
Lucho Leves <lucianoleves@gmail.com>
2021-09-22 17:09:52 -0300

    Register new periodic task for update transactions

--- django/project/app/blockchain_eth/tasks.py
+++ django/project/app/blockchain_eth/tasks.py
@@ -11,3 +11,9 @@ logger = logging.getLogger(__name__)
 def update_blocks():
     logger.info("Running update blocks.")
     BlockchainEthService.update_blocks()
+
+
+@shared_task
+def update_transactions():
+    logger.info("Running update transactions.")
+    BlockchainEthService.update_transactions()
--- django/project/app/celery.py
+++ django/project/app/celery.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from celery import Celery
 
@@ -15,3 +16,18 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
+beat_max_loop_interval = 5
+
+app.conf.beat_schedule = {
+    "update_products": {
+        "task": "app.blockchain_eth.tasks.update_blocks",
+        "schedule": timedelta(seconds=30),
+    },
+    "update_transactions": {
+        "task": "app.blockchain_eth.tasks.update_transactions",
+        "schedule": timedelta(seconds=31),
+    },
+}
+app.conf.timezone = "UTC"
