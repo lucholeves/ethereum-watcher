3ba373685b8fabd0914fedc04b210526c009ea8a
Lucho Leves <lucianoleves@gmail.com>
2021-09-27 15:49:21 -0300

    Blockchain eth app: add serializers for normal and internal Transactions

--- django/project/app/blockchain_eth/serializers.py
+++ django/project/app/blockchain_eth/serializers.py
@@ -27,10 +27,60 @@ class BlockModelSerializer(serializers.ModelSerializer):
         return date_time
 
 
-class TransactionModelSerializer(serializers.ModelSerializer):
+class TransactionInternalModelSerializer(serializers.ModelSerializer):
+    to_id = serializers.ReadOnlyField(source="to")
+    from_id = serializers.ReadOnlyField(source="from")
+    gas = serializers.ReadOnlyField()
+    hash = serializers.ReadOnlyField()
+    type = serializers.ReadOnlyField()
+    input = serializers.ReadOnlyField()
+    value = serializers.ReadOnlyField()
+    errCode = serializers.ReadOnlyField()
+    gasUsed = serializers.ReadOnlyField()
+    isError = serializers.ReadOnlyField()
+    traceId = serializers.ReadOnlyField()
+    timeStamp = serializers.ReadOnlyField()
+    timeStampDateTime = serializers.SerializerMethodField()
+    blockNumber = serializers.ReadOnlyField()
+    contractAddress = serializers.ReadOnlyField()
+
     class Meta:
         model = Transaction
-        fields = "__all__"
+        exclude = ["block"]
+
+    def to_representation(self, instance):
+        representation = instance.data
+        return super().to_representation(representation)
+
+    def get_timeStampDateTime(self, obj):
+        date_time = datetime.utcfromtimestamp(int(obj["timeStamp"]))
+        return date_time
+
+
+class TransactionNormalModelSerializer(serializers.ModelSerializer):
+    to_id = serializers.ReadOnlyField(source="to")
+    from_id = serializers.ReadOnlyField(source="from")
+    r = serializers.ReadOnlyField()
+    s = serializers.ReadOnlyField()
+    v = serializers.ReadOnlyField()
+    gas = serializers.ReadOnlyField()
+    hash = serializers.ReadOnlyField()
+    type = serializers.ReadOnlyField()
+    input = serializers.ReadOnlyField()
+    nonce = serializers.ReadOnlyField()
+    value = serializers.ReadOnlyField()
+    chainId = serializers.ReadOnlyField()
+    gasPrice = serializers.ReadOnlyField()
+    blockHash = serializers.ReadOnlyField()
+    accessList = serializers.ReadOnlyField()
+    blockNumber = serializers.ReadOnlyField()
+    maxFeePerGas = serializers.ReadOnlyField()
+    transactionIndex = serializers.ReadOnlyField()
+    maxPriorityFeePerGas = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Transaction
+        exclude = ["block"]
 
     def to_representation(self, instance):
         representation = instance.data
--- django/project/app/blockchain_eth/urls.py
+++ django/project/app/blockchain_eth/urls.py
@@ -1,15 +1,60 @@
-from django.urls import include, path
+from django.urls import path
 
-from rest_framework import routers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-from .views import BlockViewset, TransactionViewset
+from .views import (
+    BlockListAPIView,
+    TransactionInternalHistoryByAddress,
+    TransactionInternalListAPIView,
+    TransactionNormalHistoryByAddress,
+    TransactionNormalListAPIView,
+)
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
 
-router.register(r"blocks", BlockViewset)
-router.register(r"transactions", TransactionViewset)
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "blocks": reverse("blocks-list", request=request, format=format),
+            "transactions-normal": reverse(
+                "transactions-normal-list", request=request, format=format
+            ),
+            "transactions-internal": reverse(
+                "transactions-internal-list", request=request, format=format
+            ),
+        }
+    )
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", api_root),
+    # -- Blocks
+    path("blocks/", BlockListAPIView.as_view(), name="blocks-list"),
+    # -- Transactions
+    # -------- Normals
+    path(
+        "transactions/normal/",
+        TransactionNormalListAPIView.as_view(),
+        name="transactions-normal-list",
+    ),
+    path(
+        "transactions/normal/address-history/<str:address>/",
+        TransactionNormalHistoryByAddress.as_view(),
+        name="transactions-normal-address-history-list",
+    ),
+    # -------- Internals
+    path(
+        "transactions/internal/",
+        TransactionInternalListAPIView.as_view(),
+        name="transactions-internal-list",
+    ),
+    path(
+        "transactions/internal/address-history/<str:address>",
+        TransactionInternalHistoryByAddress.as_view(),
+        name="transactions-internal-address-history-list",
+    ),
+]
