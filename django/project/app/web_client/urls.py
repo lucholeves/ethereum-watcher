0f9565e31deb16e29dc3027367e79ff885229221
Lucho Leves <lucianoleves@gmail.com>
2021-09-27 15:49:45 -0300

    Web client: add new templates

--- django/project/app/web_client/urls.py
+++ django/project/app/web_client/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-from .view import index
+from .view import address_history, block_details, index
 
-urlpatterns = [path("", index)]
+urlpatterns = [
+    path("", index),
+    path("block/<int:block_number>", block_details),
+    path("address/<str:address>", address_history),
+]
--- django/project/app/web_client/view.py
+++ django/project/app/web_client/view.py
@@ -6,3 +6,15 @@ def index(request):
     template = loader.get_template("web_client/index.html")
     context = {}
     return HttpResponse(template.render(context, request))
+
+
+def block_details(request, block_number):
+    template = loader.get_template("web_client/block_transactions.html")
+    context = {"block_number": block_number}
+    return HttpResponse(template.render(context, request))
+
+
+def address_history(request, address):
+    template = loader.get_template("web_client/address_history.html")
+    context = {"address": address}
+    return HttpResponse(template.render(context, request))
